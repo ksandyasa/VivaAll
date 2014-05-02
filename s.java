ad52c5d8d9bf0f36141a3642e1fa6f0ea3d3800e
ksandyasa <ksandyasa@gmail.com>
2014-05-02 15:58:37 +0700

    made some changes

--- s.java
+++ s.java
@@ -2,5 +2,38 @@ public class Social {
     protected String id, name, email;
     
     public Social () {
+        id = "";
+        name = "";
+        email = "";
+    }
+    
+    public Social(String i, String n, String e) {
+        id = i;
+        name = n;
+        email = e;
+    }
+    
+    public void setId(String i) {
+        id = i;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setName(String n) {
+        name = n;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setEmail(String e) {
+        email = e;
+    }
+    
+    public String getEmail() {
+        return email;
     }
 }
